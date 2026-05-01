465b79239e4c50e3afa81c995db7b6945c802e34
Victormrf <vmrf2000@hotmail.com>
2026-05-01 02:42:19 -0300

    Created swagger embedded documentation

--- nestjs-api/src/main.ts
+++ nestjs-api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,15 @@ async function bootstrap() {
     origin: ["https://xplore-hub.vercel.app", "http://localhost:3001"],
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Events Management API')
+    .setDescription('The Events Management API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
